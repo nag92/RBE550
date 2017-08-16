6d90a6e2a7b7adad2dd089b5238ea505eb1a26b4
ngoldfarb <nagoldfarb@wpi.edu>
2017-08-16 09:09:27 -0400

    import moved to proper place

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -5,10 +5,11 @@
 #code based on the simplemanipulation.py example
 import time
 import openravepy
-import numpy as np
+
 
 #### YOUR IMPORTS GO HERE ####
-
+import numpy as np
+import astar
 #### END OF YOUR IMPORTS ####
 
 if not __openravepy_build_doc__:
@@ -54,7 +55,7 @@ if __name__ == "__main__":
 
         #### Implement the A* algorithm to compute a path for the robot's base starting from the current configuration of the robot and ending at goalconfig. The robot's base DOF have already been set as active. It may be easier to implement this as a function in a separate file and call it here.
         goal = np.array([goalconfig[0], goalconfig[1], goalconfig[2]])
-        
+
 
         #### Draw your path in the openrave here (see /usr/lib/python2.7/dist-packages/openravepy/_openravepy_0_8/examples/tutorial_plotting.py for examples)
 
