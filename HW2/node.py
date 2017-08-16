e92d454e3535e8d7cf9dba8bfa3206b710586b8f
ngoldfarb <nagoldfarb@wpi.edu>
2017-08-16 10:54:39 -0400

    added default parameter

--- HW2/node.py
+++ HW2/node.py
@@ -4,7 +4,7 @@ class Node(object):
         A node is a point on the map along with all information on how that
         point was added to the graph
     """
-    def __init__(self, loc):
+    def __init__(self, loc=None):
         self._loc = loc
 
         self._cost = 0.0
